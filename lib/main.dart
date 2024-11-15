76f9006ed9a512164b490b0bf2b99f03f0e6cad4
MSI <kyawlinthant128380@gmail.com>
2024-11-15 22:24:09 +0700

    Web3 Auth Login from Documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,360 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:web3auth_flutter/enums.dart';
+import 'package:web3auth_flutter/input.dart';
+import 'package:web3auth_flutter/output.dart';
+import 'package:web3auth_flutter/web3auth_flutter.dart';
+
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  String _result = '';
+  bool logoutVisible = false;
+  String rpcUrl = 'https://rpc.ankr.com/eth_sepolia';
+  // TextEditingController for handling input from the text field
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    initPlatformState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(final AppLifecycleState state) {
+    // This is important to trigger the on Android.
+    if (state == AppLifecycleState.resumed) {
+      Web3AuthFlutter.setCustomTabsClosed();
+    }
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    Uri redirectUrl;
+    String clientId =
+        'BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ';
+    if (Platform.isAndroid) {
+      redirectUrl = Uri.parse('w3a://com.example.w3aflutter/auth');
+    } else if (Platform.isIOS) {
+      redirectUrl = Uri.parse('com.example.w3aflutter://auth');
+    } else {
+      throw UnKnownException('Unknown platform');
+    }
+
+    await Web3AuthFlutter.init(Web3AuthOptions(
+      clientId: clientId,
+      network: Network.sapphire_mainnet,
+      redirectUrl: redirectUrl,
+      buildEnv: BuildEnv.production,
+      // 259200 allows user to stay authenticated for 3 days with Web3Auth.
+      // Default is 86400, which is 1 day.
+      sessionTime: 259200,
+    ));
+
+    await Web3AuthFlutter.initialize();
+
+    final String res = await Web3AuthFlutter.getPrivKey();
+    log(res);
+    if (res.isNotEmpty) {
+      setState(() {
+        logoutVisible = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Web3Auth x Flutter QuickStart'),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+              Visibility(
+                visible: !logoutVisible,
+                child: Column(
+                  children: [
+                    const Icon(
+                      Icons.flutter_dash,
+                      size: 80,
+                      color: Color(0xFF1389fd),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    const Text(
+                      'Web3Auth',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 36,
+                        color: Color(0xFF0364ff),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Welcome to Web3Auth x Flutter Quick Start Demo',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Login with',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // Text field for entering the user's email
+                    TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Email',
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _login(
+                        () => _withEmailPasswordless(emailController.text),
+                      ),
+                      child: const Text('Login with Email Passwordless'),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButtonTheme(
+                data: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 195, 47, 233),
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+                child: Visibility(
+                  visible: logoutVisible,
+                  child: Column(
+                    children: [
+                      Center(
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red[600],
+                              foregroundColor: Colors.white,
+                            ),
+                            onPressed: _logout(),
+                            child: const Column(
+                              children: [
+                                Text('Logout'),
+                              ],
+                            )),
+                      ),
+                      const Text(
+                        'Blockchain calls',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      ElevatedButton(
+                        onPressed: _getUserInfo,
+                        child: const Text('Get UserInfo'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _getAddress,
+                        child: const Text('Get Address'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _getBalance,
+                        child: const Text('Get Balance'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _sendTransaction,
+                        child: const Text('Send Transaction'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(_result),
+              )
+            ],
+          )),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  VoidCallback _login(Future<Web3AuthResponse> Function() method) {
+    return () async {
+      try {
+        final Web3AuthResponse response = await method();
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('privateKey', response.privKey.toString());
+        setState(() {
+          _result = response.toString();
+          logoutVisible = true;
+        });
+      } on UserCancelledException {
+        log("User cancelled.");
+      } on UnKnownException {
+        log("Unknown exception occurred");
+      }
+    };
+  }
+
+  VoidCallback _logout() {
+    return () async {
+      try {
+        setState(() {
+          _result = '';
+          logoutVisible = false;
+        });
+        await Web3AuthFlutter.logout();
+      } on UserCancelledException {
+        log("User cancelled.");
+      } on UnKnownException {
+        log("Unknown exception occurred");
+      }
+    };
+  }
+
+  Future<Web3AuthResponse> _withEmailPasswordless(String userEmail) async {
+    try {
+      log(userEmail);
+      return await Web3AuthFlutter.login(LoginParams(
+        loginProvider: Provider.email_passwordless,
+        extraLoginOptions: ExtraLoginOptions(login_hint: userEmail),
+      ));
+    } catch (e) {
+      log("Error during email/passwordless login: $e");
+      // Handle the error as needed
+      // You might want to show a user-friendly message or log the error
+      return Future.error("Login failed");
+    }
+  }
+
+  Future<TorusUserInfo> _getUserInfo() async {
+    try {
+      TorusUserInfo userInfo = await Web3AuthFlutter.getUserInfo();
+      log(userInfo.toString());
+      setState(() {
+        _result = userInfo.toString();
+      });
+      return userInfo;
+    } catch (e) {
+      log("Error during email/passwordless login: $e");
+      // Handle the error as needed
+      // You might want to show a user-friendly message or log the error
+      return Future.error("Login failed");
+    }
+  }
+
+  Future<String> _getAddress() async {
+    final prefs = await SharedPreferences.getInstance();
+    final privateKey = prefs.getString('privateKey') ?? '0';
+
+    final credentials = EthPrivateKey.fromHex(privateKey);
+    final address = credentials.address;
+    log("Account, ${address.hexEip55}");
+    setState(() {
+      _result = address.hexEip55.toString();
+    });
+    return address.hexEip55;
+  }
+
+  Future<EtherAmount> _getBalance() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final privateKey = prefs.getString('privateKey') ?? '0';
+
+      final client = Web3Client(rpcUrl, Client());
+      final credentials = EthPrivateKey.fromHex(privateKey);
+      final address = credentials.address;
+
+      // Get the balance in wei
+      final weiBalance = await client.getBalance(address);
+
+      // Convert wei to ether
+      final etherBalance = EtherAmount.fromBigInt(
+        EtherUnit.ether,
+        weiBalance.getInEther,
+      );
+
+      log(etherBalance.toString());
+
+      setState(() {
+        _result = etherBalance.toString();
+      });
+
+      return etherBalance;
+    } catch (e) {
+      // Handle errors as needed
+      log("Error getting balance: $e");
+      return EtherAmount.zero();
+    }
+  }
+
+  Future<String> _sendTransaction() async {
+    final prefs = await SharedPreferences.getInstance();
+    final privateKey = prefs.getString('privateKey') ?? '0';
+
+    final client = Web3Client(rpcUrl, Client());
+    final credentials = EthPrivateKey.fromHex(privateKey);
+    final address = credentials.address;
+    try {
+      final receipt = await client.sendTransaction(
+        credentials,
+        Transaction(
+          from: address,
+          to: EthereumAddress.fromHex(
+            '0xeaA8Af602b2eDE45922818AE5f9f7FdE50cFa1A8',
+          ),
+          // gasPrice: EtherAmount.fromUnitAndValue(EtherUnit.gwei, 100),
+          value: EtherAmount.fromInt(
+            EtherUnit.gwei,
+            5000000,
+          ), // 0.005 ETH
+        ),
+        chainId: 11155111,
+      );
+      log(receipt);
+      setState(() {
+        _result = receipt;
+      });
+      return receipt;
+    } catch (e) {
+      setState(() {
+        _result = e.toString();
+      });
+      return e.toString();
+    }
   }
 }
